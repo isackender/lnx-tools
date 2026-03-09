0e7f759f23571ac39dea558ca87d67c83d4bff8f
Isackender <isack.info@gmail.com>
2026-03-09 11:20:54 +0100

    Create install-scripts.sh

---
+++ install-scripts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$0")/scripts"
+TARGET_DIR="/usr/local/bin"
+
+# Install script for backup/restore nginx
+install -m 0755 "$SCRIPT_DIR/backup-nginx" "$TARGET_DIR/backup-nginx"
+install -m 0755 "$SCRIPT_DIR/restore-nginx" "$TARGET_DIR/restore-nginx"
+
+printf "\n--\nScripts installed in %s, Neo.\n" "$TARGET_DIR"
