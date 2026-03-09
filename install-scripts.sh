d305f22cd804c26c25b90b482ca7b591b49fc971
Isackender <isack.info@gmail.com>
2026-03-09 13:08:34 +0100

    Update install-scripts.sh

--- install-scripts.sh
+++ install-scripts.sh
@@ -9,4 +9,4 @@ TARGET_DIR="/usr/local/bin"
 install -m 0755 "$SCRIPT_DIR/backup-nginx" "$TARGET_DIR/backup-nginx"
 install -m 0755 "$SCRIPT_DIR/restore-nginx" "$TARGET_DIR/restore-nginx"
 
-printf "\n--\nScripts installed in %s, Neo.\n" "$TARGET_DIR"
+printf "\n--\nScripts installed in %s\n" "$TARGET_DIR"
