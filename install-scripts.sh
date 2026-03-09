fb2479f1c80bff399ccc829114aae3d5d896a0ed
Isackender <isack.info@gmail.com>
2026-03-09 11:38:29 +0100

    Update install-scripts.sh

--- install-scripts.sh
+++ install-scripts.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$0")/scripts"
 TARGET_DIR="/usr/local/bin"
 
-# Install script for backup/restore nginx
+# Install scripts to backup/restore nginx
 install -m 0755 "$SCRIPT_DIR/backup-nginx" "$TARGET_DIR/backup-nginx"
 install -m 0755 "$SCRIPT_DIR/restore-nginx" "$TARGET_DIR/restore-nginx"
 
